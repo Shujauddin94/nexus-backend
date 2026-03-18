0d6271cdc0f0742ef2d8d8adf112c3135ea42719
Shujauddin94 <shujauddin9432@gmail.com>
2026-03-18 23:10:36 +0500

    added js

--- middleware/async.js
+++ middleware/async.js
@@ -1,4 +1,4 @@
 const asyncHandler = fn => (req, res, next) =>
     Promise.resolve(fn(req, res, next)).catch(next);
 
-module.exports = asyncHandler;
+module.exports = asyncHandler; // This middleware allows us to write our route handlers as async functions and automatically catches any errors, passing them to the next middleware (which is typically an error handler).
